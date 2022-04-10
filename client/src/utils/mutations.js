7eb03c88289083ec456a09bb45682690bfef0011
Zachary Clark <zacclark369@gmail.com>
2022-04-10 09:14:06 -0700

    finished mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,17 +2,47 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const ADD_USER = gql`
-
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const SAVE_BOOK = gql`
-
+mutation saveBook($authors: [String!], $description: String!, $title: String!, $bookId: ID!, $image: String, $link: String) {
+    bookId
+    description
+    title
+    image
+    link
+}
 `;
 
 export const REMOVE_BOOK = gql`
-
+mutation removeBook($bookId: ID!) {
+  removeBook(bookId: $bookId) {
+    bookId
+    authors
+    description
+    title
+    image
+  }
+}
 `;
